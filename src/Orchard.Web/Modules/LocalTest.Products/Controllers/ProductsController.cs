b675bad9e3f2f3c1e16dbc5f3ee2960fd8614f93
huaqin.tang <huaqin.tang@aivics.com>
2018-07-02 19:08:35 +0800

    <LoaclTest>   women页面

---
+++ src/Orchard.Web/Modules/LocalTest.Products/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Orchard.Themes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocalTest.Products.Controllers
+{
+    [Themed]
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Women
+        public ActionResult WomenIndex()
+        {
+            return View();
+        }
+
+        //Men
+        public ActionResult MenIndex()
+        {
+            return View();
+        }
+
+    }
+}
